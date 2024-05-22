d1618d352e44115ed3deb52ae8c3db0e7dfa693c
Lakhan <lakhansoni126@gmail.com>
2024-05-22 16:33:22 +0530

    google auth

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,20 @@
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, googleProvider } from '../../firebase';
 
 const Login = () => {
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleGoogleSignIn = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+            navigate('/');
+        } catch (error) {
+            setError(error.message);
+        }
+    };
     return (
         <>
             <section className="bg-[#191825] h-screen">
@@ -16,7 +30,8 @@ const Login = () => {
                         <h1 className="font-bold py-2 text-[24px]">Parking Management </h1>
                         <h1 className="text-[18px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, laboriosam?</h1>
                         <h1 className="text-[18px]">Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
-                        <button className="bg-[#865DFF] font-bold text-[17px] px-4 p-1 rounded-[20px] hover:bg-[#6836fe] my-5" >Login</button>
+                        {error && <p>Error: {error}</p>}
+                        <button onClick={handleGoogleSignIn} className="bg-[#865DFF] font-bold text-[17px] px-4 p-1 rounded-[20px] hover:bg-[#6836fe] my-5" >Login</button>
 
                     </div>
                     <div>
@@ -29,4 +44,4 @@ const Login = () => {
     )
 }
 
-export default  Login
+export default Login
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDlClLuG20YYd2jK6MCgwsHB3w6sS0ms2U",
@@ -14,5 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-export { db, auth }
+
+export { db, auth, googleProvider }
