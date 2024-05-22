86d8fd6f434698361892543d3e4d1c2705c5c471
hemantsony09 <hemantsoni252004@gmail.com>
2024-05-22 17:19:06 +0530

    new logo

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -6,13 +6,14 @@ const Signup = () => {
             <section className="bg-[#191825] h-screen">
                 <nav className="bg-[#865DFF] flex text-white  justify-between text-center px-10 items-center">
 
-                    <div><img className="w-14" src="../../../public/images/logo.png" alt="" /></div>
+                    <div><img className="w-14" src="../../../images/logo.png" alt="" /></div>
                     <div><ul><li className=" hover:scale-[1.09] px-[20px] cursor-pointer ">Login</li></ul></div>
 
                 </nav>
 
                 <div className="flex justify-around mt-[20vh]" >
-                    <div className="text-white py-20">
+                    <div className ="text-white py-20">
+
                         <h1 className="font-bold text-[24px]">Parking Management </h1>
                         <h1 className="text-[18px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, laboriosam?</h1>
                         <h1 className="text-[18px]">Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
