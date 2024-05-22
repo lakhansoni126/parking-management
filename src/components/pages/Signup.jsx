7d99c604ce93aa7bd501a722d12c98450c2dc9b0
hemantsony09 <hemantsoni252004@gmail.com>
2024-05-22 15:58:37 +0530

    signup

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -3,30 +3,25 @@
 const Signup = () => {
     return (
         <>
-            <section className="bg-[#344e41] h-screen">
-            <nav className="bg-[#588157] flex text-white  justify-between text-center px-10 items-center">
+            <section className="bg-[#191825] h-screen">
+                <nav className="bg-[#865DFF] flex text-white  justify-between text-center px-10 items-center">
 
-                <div><img className="w-14" src="../../../public/images/logo.png" alt="" /></div>
+                    <div><img className="w-14" src="../../../public/images/logo.png" alt="" /></div>
+                    <div><ul><li className=" hover:scale-[1.09] px-[20px] cursor-pointer ">Login</li></ul></div>
 
-            </nav>
+                </nav>
 
-                <div className="flex justify-around mt-[30vh]" >
-                    <div>
-                        <h1>Parking Management </h1>
+                <div className="flex justify-around mt-[20vh]" >
+                    <div className="text-white py-20">
+                        <h1 className="font-bold text-[24px]">Parking Management </h1>
+                        <h1 className="text-[18px]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, laboriosam?</h1>
+                        <h1 className="text-[18px]">Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+                        <button className="bg-[#865DFF] text-[20px] px-4 p-1 rounded-[20px] my-5" >Login</button>
+
                     </div>
-                    <form className="" action="">
-                        <label className="block" for="name">Name</label>
-                        <input className="block" type="text" name="name" id="name" />
-
-                        <label className="block" for="email">Email</label>
-                        <input className="block" type="email" name="email" id="email" />
-
-                        <label className="block" for="password">Password</label>
-                        <input className="block" type="password" name="password" id="password" />
-
-                        <button type="submit">Login</button>
-
-                    </form>
+                    <div>
+                        <img className="w-96 border-2 rounded-lg" src="../../../images/logo.png" alt="" />
+                    </div>
                 </div>
 
             </section>
