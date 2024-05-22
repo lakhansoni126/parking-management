802997e2b28ba4fc96f374512c552394abc3a930
hemantsony09 <hemantsoni252004@gmail.com>
2024-05-22 15:20:53 +0530

    basic signup

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -2,9 +2,35 @@
 
 const Signup = () => {
     return (
-        <div>
-            Signup
-        </div>
+        <>
+            <section className="bg-[#344e41] h-screen">
+            <nav className="bg-[#588157] flex text-white  justify-between text-center px-10 items-center">
+
+                <div><img className="w-14" src="../../../public/images/logo.png" alt="" /></div>
+
+            </nav>
+
+                <div className="flex justify-around mt-[30vh]" >
+                    <div>
+                        <h1>Parking Management </h1>
+                    </div>
+                    <form className="" action="">
+                        <label className="block" for="name">Name</label>
+                        <input className="block" type="text" name="name" id="name" />
+
+                        <label className="block" for="email">Email</label>
+                        <input className="block" type="email" name="email" id="email" />
+
+                        <label className="block" for="password">Password</label>
+                        <input className="block" type="password" name="password" id="password" />
+
+                        <button type="submit">Login</button>
+
+                    </form>
+                </div>
+
+            </section>
+        </>
     )
 }
 
