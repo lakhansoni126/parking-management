b44299dea795e13b07b797cacc1a8f288718c78c
hemantsony09 <hemantsoni252004@gmail.com>
2024-05-23 12:57:46 +0530

    added css

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -3,51 +3,73 @@ import React from 'react';
 function Profile() {
     return (
         <>
-        <section className=' h-screen bg-[#191825] flex justify-center items-center'>
+        <section className='h-screen bg-[#191825] flex flex-col justify-center items-center'>
+    <div>
 
-            <img src="" alt="" />
+            <img className='invert w-40 rounded-full' src="../../../public/images/user.png" alt="" />
+    </div>        
+            <div>
+                
             <div className='flex flex-col text-white'>
-            <label className=''>
-                Name <br />
-                <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="text" placeholder="Enter your name" />
-                <input type="text" className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' placeholder="Enter another name" />
-            </label>
+                <label className=''>
+                    Name
+                    <div>
+                        <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="text" placeholder="Enter your name" />
+             
+                        <input type="text" className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' placeholder="Enter another name" />
+                
+                    </div>
+                </label>
 
-            <label className=''>
-                Mobile number
-                <br />
-                <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="number" placeholder="Enter your mobile number" />
-            </label>
+                <div>
+                    <label className=''>
+                        Mobile number
+                        <div>
+                            <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="number" placeholder="Enter your mobile number" />
+                        </div>
+                    </label>
+                </div>
 
-<div className='flex'>
-            <label className=''>
-                Office/Flat
-                <br />
-                <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="text" placeholder="Enter your office/flat number" />
-            </label>
+                <div className='flex'>
+                    <label className=''>
+                        Office/Flat
+                        <div>
+                            <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="text" placeholder="Enter your office/flat number" />
+                        </div>
+                    </label>
 
-            <label className='mx-5'>
-                Vehicle
-                <br />
-                <select className=' bg-transparent border-white border-b-[1px] p-1 my-5 ' name="Vehicle">
-                    <option className=' bg-transparent  text-black' value="" disabled selected>Select your vehicle</option>
-                    <option className=' bg-transparent  text-black' value="Car">Car</option>
-                    <option className=' bg-transparent  text-black' value="Bike">Bike</option>
-                </select>
-            </label>
-</div>
+                    <label className='mx-5'>
+                        Vehicle
+                        <div>
+                            <select className='bg-transparent w-[250px] border-[#3F0071] border-b-[2px] p-1 my-4' name="Vehicle">
+                                <option className='bg-transparent text-black' value="" disabled selected>Select your vehicle</option>
+                                <option className='bg-transparent text-black' value="Car">Car</option>
+                                <option className='bg-transparent text-black' value="Bike">Bike</option>
+                            </select>
+                        </div>
+                    </label>
+                </div>
 
-            <label className=''>
-                Model
-                <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="text" placeholder="Enter vehicle model" />
-            </label>
+                <div>
+                    <label className=''>
+                        Model
+                        <div>
+                            <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="text" placeholder="Enter vehicle model" />
+                        </div>
+                    </label>
+                </div>
 
-            <label className=''>
-                Vehicle number
-                <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="text" placeholder="Enter vehicle number" />
-            </label>
-        </div>
-
+                <div>
+                    <label className=''>
+                        Vehicle number
+                        <div>
+                            <input className='bg- border-b-2 border-[#3F0071] bg-transparent my-5 mx-5' type="text" placeholder="Enter vehicle number" />
+                        </div>
+                    </label>
+                </div>
+                <button className="bg-[#865DFF] font-bold text-[17px] px-4 p-1 rounded-[20px] hover:bg-[#6836fe] my-5">Submit</button>
+            </div>
+            </div>
         </section>
         </>
     );
