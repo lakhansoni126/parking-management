43bfd72f6dbeee13a56a4f925c0746a44acae5fa
hemantsony09 <hemantsoni252004@gmail.com>
2024-05-23 12:11:20 +0530

    profile

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 function Profile() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            <label >Name<input type="text" /></label> <input type="text" />
+
+            <label >Mobile number<input type="number" /></label>
+
+            <label > Office/Flat <input type="text" /></label>
+
+            <label >Vehicle
+                <select name="Vehicle">
+                    <option value="Car">Car</option>
+                    <option value="Bike">Bike</option>
+                </select></label>
+
+            <label >Model<input type="text" /></label>
+            
+            <label >Vehicle number<input type="text" /></label>
+
+        </>
+    );
 }
 
-export default Profile
+export default Profile;
