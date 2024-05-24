cca4d324f78f005543e0379203bdb3b3844ff3ac
hemantsony09 <hemantsoni252004@gmail.com>
2024-05-24 11:55:41 +0530

    created guard form

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -42,13 +42,13 @@ function Profile() {
                 <div>
                     <div className='flex flex-col pl-[40px] text-white'>
                         <label>
-                            Name
+                            Name*
                             <div>
                                 <input
                                     onChange={handleChange}
                                     value={formData.name}
                                     name="name"
-                                    className='w-[400px] border-b-2 border-[#3F0071] bg-transparent my-5 mr-5 mb-3'
+                                    className='w-[400px] border-b-2 border-[#3F0071] bg-transparent mb-7 mr-5 '
                                     type="text"
                                     placeholder={user.displayName}
                                 />
@@ -56,13 +56,13 @@ function Profile() {
                         </label>
                         <div>
                             <label>
-                                Mobile number
+                                Mobile number(option)
                                 <div>
                                     <input
                                         onChange={handleChange}
                                         value={formData.contactInfo}
                                         name="mn"
-                                        className='w-[400px] noscroll border-b-2 border-[#3F0071] bg-transparent my-5 mr-5 mb-3'
+                                        className='w-[400px] noscroll border-b-2 border-[#3F0071] bg-transparent mb-7 mr-5 '
                                         type="number"
                                         placeholder="mobile number"
                                     />
@@ -70,13 +70,13 @@ function Profile() {
                             </label>
                         </div>
                         <label>
-                            Office/Flat
+                            Office/Flat*
                             <div>
                                 <input
                                     onChange={handleChange}
                                     value={formData.office}
                                     name="office"
-                                    className='w-[400px] border-b-2 border-[#3F0071] bg-transparent my-5 mr-5 mb-3'
+                                    className='w-[400px] border-b-2 border-[#3F0071] bg-transparent mb-7 mr-5 '
                                     type="text"
                                     placeholder="Enter your office/flat number"
                                 />
@@ -85,13 +85,13 @@ function Profile() {
 
                         <div>
                             <label>
-                                Vehicle number
+                                Vehicle number*
                                 <div>
                                     <input
                                         onChange={handleChange}
                                         value={formData.vehicleNum}
                                         name="vn"
-                                        className='w-[400px] border-b-2 border-[#3F0071] bg-transparent my-5 mr-5 mb-3'
+                                        className='w-[400px] border-b-2 border-[#3F0071] bg-transparent mb-7 mr-5 '
                                         type="text"
                                         placeholder="Enter vehicle number"
                                     />
@@ -100,13 +100,13 @@ function Profile() {
                         </div>
                         <div>
                             <label>
-                                Vehicle
+                                Vehicle*
                                 <div>
                                     <select
                                         onChange={handleChange}
                                         value={formData.vehicleType}
                                         name="vt"
-                                        className='bg-transparent border-[#3F0071] border-b-[2px] p-1 my-4'
+                                        className='bg-transparent border-[#3F0071] border-b-[2px] p-1 mb-7'
                                     >
                                         <option value="" disabled>Select your vehicle</option>
                                         <option value="Car">Car</option>
@@ -115,7 +115,7 @@ function Profile() {
                                 </div>
                             </label>
                         </div>
-                        <button onClick={handleSubmit} className="bg-[#865DFF] font-bold text-[17px] px-4 p-1 rounded-[20px] hover:bg-[#6836fe] my-5">
+                        <button onClick={handleSubmit} className="bg-[#865DFF] font-bold text-[17px] px-4 p-1 rounded-[20px] hover:bg-[#6836fe] mb-7">
                             Submit
                         </button>
                     </div>
