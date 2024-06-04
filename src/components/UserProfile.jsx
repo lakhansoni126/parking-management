1e49979c5f6c5f314c527a3172dd89975eb22ef4
hemantsony09 <hemantsoni252004@gmail.com>
2024-06-04 11:26:20 +0530

    updated userprofile

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -34,9 +34,9 @@ function UserProfile({ initialValues, onSubmit }) {
         >
             {({ setFieldValue }) => (
                 <Form>
-                    <section className='min-h-screen bg-[#191825] flex flex-col justify-center items-center'>
+                    <section id='UserProfile' className='min-h-screen bg-[#373A40] flex flex-col justify-center items-center'>
                         <div>
-                            <div className='flex flex-col pl-[40px] text-white'>
+                            <div className='flex flex-col text-[#EEEEEE]'>
                                 <label>
                                     Name*
                                     <div>
@@ -45,7 +45,7 @@ function UserProfile({ initialValues, onSubmit }) {
                                             name="name"
                                             type="text"
                                             placeholder="Enter Your Name"
-                                            className='w-[400px] border-b-2 border-[#3F0071] bg-transparent mb-7 mr-5'
+                                            className='w-[400px] border-b-2 border-[#DC5F00] bg-transparent mb-7 mr-5'
                                         />
                                     </div>
                                 </label>
@@ -58,7 +58,7 @@ function UserProfile({ initialValues, onSubmit }) {
                                                 name="contactInfo"
                                                 type="text"
                                                 placeholder="Mobile number"
-                                                className='w-[400px] border-b-2 border-[#3F0071] bg-transparent mb-7 mr-5'
+                                                className='w-[400px] border-b-2 border-[#DC5F00] bg-transparent mb-7 mr-5'
                                             />
                                         </div>
                                     </label>
@@ -72,7 +72,7 @@ function UserProfile({ initialValues, onSubmit }) {
                                                 as="select"
                                                 name="building"
                                                 required
-                                                className='w-[400px] border-b-2 border-[#3F0071] bg-[#252437] mb-7 mr-5'
+                                                className='w-[400px] border-b-2 border-[#DC5F00] p-2 bg-[#252437] mb-7 mr-5'
                                             >
                                                 <option value="" disabled>Select your building</option>
                                                 {buildingNames.map((buildingName, index) => (
@@ -92,7 +92,7 @@ function UserProfile({ initialValues, onSubmit }) {
                                             name="officeNum"
                                             type="text"
                                             placeholder="Enter your office/flat number"
-                                            className='w-[400px] border-b-2 border-[#3F0071] bg-transparent mb-7 mr-5'
+                                            className='w-[400px] border-b-2 border-[#DC5F00] bg-transparent mb-7 mr-5'
                                         />
                                     </div>
                                 </label>
@@ -105,7 +105,7 @@ function UserProfile({ initialValues, onSubmit }) {
                                                 name="vehicleNum"
                                                 type="text"
                                                 placeholder="Enter vehicle number"
-                                                className='w-[400px] border-b-2 border-[#3F0071] bg-transparent mb-7 mr-5'
+                                                className='w-[400px] border-b-2 border-[#DC5F00] bg-transparent mb-7 mr-5'
                                                 onChange={(e) => handleVehicleNumChange(e, setFieldValue)}
                                             />
                                         </div>
@@ -119,7 +119,7 @@ function UserProfile({ initialValues, onSubmit }) {
                                             <Field
                                                 as="select"
                                                 name="vehicleType"
-                                                className='bg-[#252437] border-[#3F0071] border-b-[2px] p-1 mb-7'
+                                                className='border-[#DC5F00] w-[400px] border-b-[2px] p-1 mb-7'
                                             >
                                                 <option value="" disabled>Select your vehicle</option>
                                                 <option value="Car">Car</option>
@@ -128,7 +128,7 @@ function UserProfile({ initialValues, onSubmit }) {
                                         </div>
                                     </label>
                                 </div>
-                                <button type="submit" className="bg-[#865DFF] font-bold text-[17px] px-4 p-1 rounded-[20px] hover:bg-[#6836fe] mb-7">
+                                <button type="submit" className="bg-[#DC5F00] font-bold text-[17px] px-4 p-1 rounded-[20px] hover:bg-[#686D76] mb-7">
                                     Submit
                                 </button>
                             </div>
