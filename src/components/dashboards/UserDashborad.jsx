f59a1ca87a710b749b0faffa6d8db779e2579121
hemantsony09 <hemantsoni252004@gmail.com>
2024-06-10 16:45:22 +0530

    final touch

--- src/components/dashboards/UserDashborad.jsx
+++ src/components/dashboards/UserDashborad.jsx
@@ -14,27 +14,9 @@ const UserDashborad = () => {
         <>
             <section id='User' className="h-screen  bg-[#222831]">
                 <div id='button' className="flex  text-[#EEEEEE]">
-                    <div id='navigator' className="h-screen w-[20vw] pt-5 border-r-2 border-r-[#393E46] text-[18px] font-bold">
-                        <div className="h-fit">
-                            <button
-                                className="w-full hover:bg-[#393E46] rounded-md p-5 align-text-center"
-                                onClick={() => setActiveSection('profile')}
-                            >
-                                Profile
-                            </button>
-                        </div>
-                        <div className="h-fit">
-                            <button
-                                className="w-full  hover:bg-[#393E46]  p-5 rounded-md align-text-center"
-                                onClick={() => setActiveSection('complain')}
-                            >
-                                Complain
-                            </button>
-                        </div>
-                    </div>
+                    
 
-                    {activeSection === 'profile' && userData && (
-                        <div id="User-info" className="w-[80vw] flex flex-col items-center">
+                        <div id="User-info" className="w-[100vw] flex flex-col items-center">
                             <h1 className="font-bold text-[20px] mt-[120px]">User Information</h1>
                             <div className="flex gap-4 justify-between w-[440px] text-[20px] mt-10">
                                 <h2 className="font-bold">Name :</h2>
@@ -56,21 +38,9 @@ const UserDashborad = () => {
                                 <h4 className="font-bold">Vehicle Type :</h4>
                                 <h4>{userData.vehicleType || 'N/A'}</h4>
                             </div>
-                            {/* <div><button className="bg-[#FF5722] hover:bg-[#393E46] px-7 rounded-sm py-2">Edit</button> </div> */}
                         </div>
-                    )}
-
-                    {activeSection === 'complain' && (
-                        <div id="Complain" className="w-[80vw]">
-                            <h1 className="font-bold text-[20px] pl-10 mt-[40px]">Complaints</h1>
-                            <ul className="mt-10">
-                                <li className="py-5 pl-10 hover:bg-[#393E46]">Parking is not allowed here</li>
-                                <li className="py-5 pl-10 hover:bg-[#393E46]">Your vehicle is too cheap</li>
-                                <li className="py-5 pl-10 hover:bg-[#393E46]">Swift and Baleno owners are not allowed</li>
-                                <li className="py-5 pl-10 hover:bg-[#393E46]">Can you sell your Polo for 100 rupees?</li>
-                            </ul>
-                        </div>
-                    )}
+                  
+                  
                 </div>
             </section>
         </>
